7a0953d311e87fa652c5e9f8980dbd00a37729cd
Stella Taylor <stellataylor10@gmail.com>
2021-04-28 15:53:45 +0100

    did post request and started css

--- front-end/src/components/HomePage/index.js
+++ front-end/src/components/HomePage/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import BirthdayForm from './BirthdayForm';
+import css from './HomePage.module.css';
 
 function HomePage() {
     return (
-        <div class="form-container">
-            <div class="title">
+        <div className={css.formContainer}>
+            <div className={css.title}>
                 <h1>BIRTHDAY BUDDIES</h1>
                 <h2>Because no one wants to spend their birthday alone</h2>
             </div>
